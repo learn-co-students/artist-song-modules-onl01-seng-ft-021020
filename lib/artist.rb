96af71d14c55099c5af49fd3e2a30be0a760c1e6
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-26 21:08:31 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,7 @@ require 'pry'
 class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
+  extend Findable::ClassMethods
   include Paramable::InstanceMethods
  
   attr_accessor :name
@@ -14,10 +15,6 @@ class Artist
   def initialize
     super
     @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
--- lib/song.rb
+++ lib/song.rb
@@ -4,16 +4,13 @@ require 'pry'
 class Song
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
+  extend Findable::ClassMethods
   include Paramable::InstanceMethods
   
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
 
   def self.all
     @@songs
